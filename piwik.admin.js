b370d4f6318601ac4bf5ac10824cdee6f9c6e468
hass <hass@85918.no-reply.drupal.org>
2011-02-13 01:12:51 +0000

    Rename piwik_visibility variable to piwik_visibility_pages for consistency.

--- piwik.admin.js
+++ piwik.admin.js
@@ -12,7 +12,7 @@ Drupal.behaviors.trackingSettingsSummary = {
     }
 
     $('fieldset#edit-page-vis-settings', context).drupalSetSummary(function (context) {
-      var $radio = $('input[name="piwik_visibility"]:checked', context);
+      var $radio = $('input[name="piwik_visibility_pages"]:checked', context);
       if ($radio.val() == 0) {
         if (!$('textarea[name="piwik_pages"]', context).val()) {
           return Drupal.t('Not restricted');
